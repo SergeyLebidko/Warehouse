e0cdda7d7b1f1b68694ba9dac766c7dcb99c38e7
SergeyLebidko <it.sergeyler@mail.ru>
2019-05-14 12:46:46 +0300

    В главный класс добавлены создание подключения к БД и запуск интерфейса

--- src/main/java/warehouse/MainClass.java
+++ src/main/java/warehouse/MainClass.java
@@ -1,9 +1,26 @@
 package warehouse;
 
+import javax.swing.*;
+
 public class MainClass {
 
+    private static DBHandler dbHandler;
+
     public static void main(String[] args) {
-        System.out.println("Привет!");
+        //Пытаемся получить подключение к базе данных
+        try {
+            dbHandler = new DBHandler();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Ошибка подключения к базе данных. Приложение будет закрыто.", "", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
+        //Если подключение успешно получено, то запускаем создание интерфейса
+        new GUI();
+    }
+
+    public static DBHandler getDbHandler() {
+        return dbHandler;
     }
 
 }
